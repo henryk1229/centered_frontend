545a4708698035f8740e9bda68ece702fe60263d
henryk1229 <henryk1229@gmail.com>
2019-10-17 10:02:00 -0400

    secure deploy

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = (props) => {
 
 
   //fetch config
-  const url = 'http://mystifying-wescoff-20b0b0.netlify.com/api/v1/login'
+  const url = 'https://mystifying-wescoff-20b0b0.netlify.com/api/v1/login'
   const fetchConfig = {
     method: "POST",
     headers: {
